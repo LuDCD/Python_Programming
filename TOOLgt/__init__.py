05e28c55e1c0cc70d6ef33413f019ee43431972d
zhouheng <zhouheng2018@163.com>
2018-04-16 15:20:36 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- TOOLgt/__init__.py
+++ TOOLgt/__init__.py
@@ -7,6 +7,10 @@ File function
 @author: ZHOU Heng
 """
 
+# TOOLgt模块下的所有子模块都要添加到这个列表中。
+# 以便 from TOOLgt import * 可以正常调用
+__all__ = ["file22listGT", "isNumberGT", "sortGT"]
+
 
 def test():
     pass

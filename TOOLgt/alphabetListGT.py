578c0d89748bc187a21a00bceea776d1bd113e45
zhouheng <zhouheng2018@163.com>
2018-04-17 12:28:53 +0800

    lucky

--- TOOLgt/alphabetListGT.py
+++ TOOLgt/alphabetListGT.py
@@ -2,12 +2,15 @@
 # -*- coding:utf8 -*-
 
 """
-字母表列表
+关于英文字母的操作
 
 @author: ZHOU Heng
 """
 
 def AlphabetList():
+    '''
+    :return:大写26字母列表
+    '''
     # Aord = ord( 'a' )
     # return [chr(i+Aord) for i in range(26)]
 
@@ -15,8 +18,47 @@ def AlphabetList():
 
 
 def alphabetList():
+    '''
+    :return:返回小写字母表
+    '''
     return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+def isLowletters( x ):
+    '''
+    判断是否为小写字母
+    :type x: str
+    :rtype:bool
+    '''
+    flag = 0
+    if 'a' <= x <= 'z':
+        flag = 1
+
+    return bool( flag )
+
+def ispperletters( x ):
+    '''
+    判断是否为大写字母
+    :type x: str
+    :rtype:bool
+    '''
+    flag = 0
+    if 'A' <= x <= 'Z':
+        flag = 1
+
+    return bool( flag )
+
+def isLetters( x ):
+    '''
+    是否为字母
+    :type x: str
+    :rtype: bool
+    '''
+    flag = 0
+    if 'a' <= x <= 'Z':
+        flag = 1
+
+    return bool( flag )
 
 def test():
     A = AlphabetList()

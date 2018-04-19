81ca952e0f40e87639baaa74bc22fb8d31dcff5a
zhouheng <zhouheng2018@163.com>
2018-04-19 21:54:59 +0800

    201804019-4

--- BOOK-MLinActionGTpy35/k-近邻算法/kNNGT.py
+++ BOOK-MLinActionGTpy35/k-近邻算法/kNNGT.py
@@ -9,7 +9,10 @@
 """
 
 import numpy as np
-import operator
+
+import matplotlib
+import matplotlib.pyplot as plt
+
 
 def creatDataSet():
     group = np.array( [ [1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1] ] )
@@ -64,6 +67,7 @@ def classify0( X, dataSet, labels, k ):
     return labFreq[0][0]
 
 
+# 从文件中读出数据，并处理返回
 def file2matrix( filename ):
     '''
     处理文本文件。每行有4个元素，3个特征 + 一个标签
@@ -82,23 +86,98 @@ def file2matrix( filename ):
     for i in range( numberOfLine ):
         line = listOfLines[i]
         line = line.strip()         # 除掉收尾空白字符
-        tlist = line.split('\t')    # 以水平制表符来切分字符串，返回切分后的元素列表
-        reMatrix[i:] = tlist[0:3]
+        tlist = line.split('\t')    # 以水平制表符来切分字符串，返回切分后的字符元素列表
+        reMatrix[i, :] = tlist[0:3]
         labelVector.append( tlist[-1] )
+        # 这里没有进行 int( tlist[-1] )强转为数字，得到的字符串。根据需要自己转数字。
 
     return reMatrix, labelVector
 
 
+def drawData( x, y , labels):
+    '''
+    :param x: x轴数据
+    :param y: y轴数据
+    :param labels: 分类标签数据
+    :return:
+    '''
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter( x, y, 15.0*np.array(labels), 15.0*np.array(labels) )
+    plt.show()
 
+
+# 归一化数据
+def autoNorm( dataSet ):
+    minValues = dataSet.min(0)      # 列（0轴）最小值向量。
+    maxValues = dataSet.max(0)
+
+    # 造坑
+    normDataSet = np.zeros( dataSet.shape )
+
+    # 归一化，把数填上
+    normDataSet = dataSet - np.tile( minValues, ( dataSet.shape[0], 1) )
+    minMaxDif = np.tile( maxValues - minValues, ( dataSet.shape[0], 1) )
+    normDataSet = normDataSet / minMaxDif
+
+    # 返回归一化数据。最大，最小值会在以后归一化测试数据时用到
+    return normDataSet, maxValues, minValues
+    # print( normDataSet )
+
+# 分类器测试
+def datingClassTest():
+    dataTestRatio = 0.1     # 选 10% 的数据，来测试
+
+    datingDataSet, labelsVectors = file2matrix( 'datingTestSet.txt' )    # 获得数据
+    datingDataSet, maxValues, minValues = autoNorm( datingDataSet )       # 数据归一化
+
+    m = datingDataSet.shape[0]
+
+    orrorNum = 0
+    # 选前 10%(=dataRatio*m) 作为测试
+    numTestData = int( m*dataTestRatio )    # 必须整数化，因为后面有索引
+    trainData = datingDataSet[numTestData:m, :]
+    trainLabels = labelsVectors[numTestData:m]
+    for i in range( numTestData ):
+        lab = classify0(datingDataSet[i, :], trainData, trainLabels, 3)
+        if lab != labelsVectors[i]:
+            orrorNum += 1
+
+    return orrorNum / float(numTestData)
 
 
 def test():
-    group, labels = creatDataSet()
-    print( classify0( [0, 0], group, labels, 3))
+    # group, labels = creatDataSet()
+    # print( classify0( [0, 0], group, labels, 3))
+
+
 
     # 获得约会数据
-    datingDataMat, datingLabels = file2matrix( 'datingTestSet2.txt' )
+    # datingDataMat, datingLabels = file2matrix( 'datingTestSet2.txt' )
+    # for i in range( len(datingLabels) ):
+    #     datingLabels[i] = eval( datingLabels[i] )
+
+    # 我知道 datingTestSet2.txt 里面的数据的label 是数字，
+    # 由于file2matrix,转换之后的全是字符，所以我们要变数字字符为数字，因为后面用到数字的值
     # print( datingDataMat, datingLabels)
+
+
+
+    # 可视化
+    # drawData( datingDataMat[:,0], datingDataMat[:,1], datingLabels)
+
+
+
+    # 归一化数据
+    # normDatingMat, maxVector, minVector = autoNorm( datingDataMat )
+
+
+
+    # 查看错误率
+    orrorRatio = datingClassTest()
+    print( orrorRatio )
+
+
 
 if __name__ == '__main__':
     test()

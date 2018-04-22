1f80f379e2e9ad73966b57d18869822db05f1665
zhouheng <zhouheng2018@163.com>
2018-04-22 22:20:15 +0800

    20180422-3

--- PracticePY/test789.py
+++ PracticePY/test789.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 
-print( str(100) )
+
+import numpy as np
+dataSet = [[1, 1, 'mayve'],
+           [1, 1, 'yes'],
+           [1, 0, 'no'],
+           [0, 1, 'no'],
+           [0, 1, 'no']]
+print( np.array(dataSet) )
+print( np.array(dataSet).shape )

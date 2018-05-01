a676e8876fe1d44d2d83a22d852ebb7ccc6b3eff
zhouheng <zhouheng2018@163.com>
2018-05-01 19:39:07 +0800

    20180501-5

--- BIT_MOOC-Python/爬虫/1 爬取网页的通用代码框架.py
+++ BIT_MOOC-Python/爬虫/1 爬取网页的通用代码框架.py
@@ -12,7 +12,7 @@ import requests
 def getHTMLTxt(url):
     try:
         r = requests.get(url, timeout=30)
-        r.raise_for_status()
+        r.raise_for_status()    # 如果状态码不是200，引发HTTPError异常
         r.encoding = r.apparent_encoding
         return r.text
     except :
---
+++ BIT_MOOC-Python/爬虫/3 实例1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+File function
+
+@author: ZHOU Heng
+"""
+
+# 爬取亚马逊的一个商品信息
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
+
+
+

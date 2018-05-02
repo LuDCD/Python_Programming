feba0da11fbaef20a05e54505762c6a101f68f94
zhouheng <zhouheng2018@163.com>
2018-05-02 14:14:44 +0800

    20180501-2

--- BIT_MOOC-Python/爬虫/1 爬取网页的通用代码框架.py
+++ BIT_MOOC-Python/爬虫/1 爬取网页的通用代码框架.py
@@ -10,13 +10,16 @@
 import requests
 
 def getHTMLTxt(url):
+
     try:
         r = requests.get(url, timeout=30)
         r.raise_for_status()    # 如果状态码不是200，引发HTTPError异常
         r.encoding = r.apparent_encoding
-        return r.text
+        # return r.text
+        print( r.text )
     except :
-        return '产生异常'
+        # return '产生异常'
+        print( '产生异常' )
 
 def test():
     getHTMLTxt('www.baidu.com')
@@ -24,7 +27,5 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
-
-
+    pass
 

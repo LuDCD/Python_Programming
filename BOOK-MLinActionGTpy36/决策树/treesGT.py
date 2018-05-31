323b286892a792ea3ec6dbfcb22be17499d2f625
zhouheng <zhouheng2018@163.com>
2018-05-31 14:53:18 +0800

    20180531

--- BOOK-MLinActionGTpy36/决策树/treesGT.py
+++ BOOK-MLinActionGTpy36/决策树/treesGT.py
@@ -11,14 +11,14 @@ import math
 import numpy as np
 
 # 计算数据集的 平均信息熵
-def calcShannonEntAvg( dataSet ):
-    '''
+def calcShannonEntAvg(dataSet):
+    """
     :typer dataSet: List[List[·]]
     :return: 标签的平均信息熵
-    '''
-    lm = len( dataSet )      # 数据的总个数
+    """
+    lm = len(dataSet)  # 数据的总个数
 
-    labelsDict = dict()     # 每个标签出现的次数
+    labelsDict = dict()  # 每个标签出现的次数
     # 遍历数据
     for aData in dataSet:
         key = aData[-1]
@@ -37,64 +37,61 @@ def calcShannonEntAvg( dataSet ):
 
 # 创建测试数
 def creatDataSet():
-    dataSet = [[1, 1, 'mayve'],
-               [1, 1, 'yes'],
-               [1, 0, 'no'],
-               [0, 1, 'no'],
-               [0, 1, 'no']]
+    dataSet = [[1, 1, "mayve"], [1, 1, "yes"], [1, 0, "no"], [0, 1, "no"], [0, 1, "no"]]
 
-    labels = ['no surfacing', 'flippers']
+    labels = ["no surfacing", "flippers"]
 
     return dataSet, labels
 
+
 # 按照给定特征拆分数据集
-def splitDataSet( dataSet, axis, values ):
-    '''
+def splitDataSet(dataSet, axis, values):
+    """
     :type dataSet: List[List[·]]
     :param axis: 给定的特征
     :param values: 给定的特征的值
     :return:拆分后的数据集
-    '''
+    """
     reData = []
     for aData in dataSet:
         if aData[axis] == values:
-            reData.append( aData[:axis] + aData[axis+1:] )
+            reData.append(aData[:axis] + aData[axis + 1 :])
 
     return reData
 
 
 # 选择最好的数据集划分方式
-def chooseBestFeatureToSplite( dataSet ):
-    '''
+def chooseBestFeatureToSplite(dataSet):
+    """
     :type dataSet: List[List[·]]
     :return: 划分最好的那个特征
-    '''
+    """
     # print( dataSet )
 
     # 把 dataSet 矩阵化
-    npDataSet = np.array( dataSet )
+    npDataSet = np.array(dataSet)
 
-    numFeatures = npDataSet.shape[1] - 1      # 特征的种类数
-    numData = npDataSet.shape[0]              # 数据的个数
+    numFeatures = npDataSet.shape[1] - 1  # 特征的种类数
+    numData = npDataSet.shape[0]  # 数据的个数
 
-    baseShannonEnt = calcShannonEntAvg( dataSet )
-    bestFeatureIndex = -1   # 最好分类特征
-    maxInfGain = 0          # 最大信息增益
+    baseShannonEnt = calcShannonEntAvg(dataSet)
+    bestFeatureIndex = -1  # 最好分类特征
+    maxInfGain = 0  # 最大信息增益
     # 遍历所有特征
-    for i in range( numFeatures ):
+    for i in range(numFeatures):
         # 第 i 个特征的所有值
-        featureValues = set( npDataSet[:,i] )
-        print( featureValues )
+        featureValues = set(npDataSet[:, i])
+        print(featureValues)
 
         shannEntSplited = 0
         for value in featureValues:
             # value = eval( valueStr )
-            splitedData = splitDataSet( dataSet, i, value )
+            splitedData = splitDataSet(dataSet, i, value)
             p = len(splitedData) / numData
-            shannEntSplited += p * calcShannonEntAvg( splitedData )
+            shannEntSplited += p * calcShannonEntAvg(splitedData)
 
         # 计算信息增益
-        infGain = shannEntSplited - baseShannonEnt      # 错了！！
+        infGain = shannEntSplited - baseShannonEnt  # 错了！！
         infGain = baseShannonEnt - shannEntSplited
 
         if infGain > maxInfGain:
@@ -102,9 +99,6 @@ def chooseBestFeatureToSplite( dataSet ):
             bestFeatureIndex = i
 
     return bestFeatureIndex
-
-
-
 
 
 def test():
@@ -115,13 +109,9 @@ def test():
     # 拆分数据
     # print( splitDataSet(dataSet, 0, 0) )
 
-
-
     # 最好的分组特征
     bestFeature = chooseBestFeatureToSplite(dataSet)
-    print( bestFeature )
-
-
+    print(bestFeature)
 
 
 if __name__ == "__main__":
---
+++ LeetCode_GT/744 寻找比目标字母大的最小字母.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+File function
+
+@author: ZHOU Heng
+"""
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()

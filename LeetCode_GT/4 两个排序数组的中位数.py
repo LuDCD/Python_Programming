374736cb5aedc3e93d4fb35e23db0674f0693de7
zhouheng2018@163.com <widkh2917#&>
2018-11-01 22:22:10 +0800

    111

--- LeetCode_GT/4 两个排序数组的中位数.py
+++ LeetCode_GT/4 两个排序数组的中位数.py
@@ -23,9 +23,45 @@ nums2 = [3, 4]
 @author: ZHOU Heng
 """
 
+# 引用numpy模块
+import numpy as np
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # 1、numpy法
+        #求数组a的中位数
+        # med1 = np.median(nums1)
+        # med2 = np.median(nums2)
+        # return (med1+med2)/2
+
+        # 普通方法
+        res = nums1 + nums2
+        res.sort()
+        n = len(res)//2
+        return res[n] if len(res)%2 ==1 else  (res[n]+res[n-1])/2
+
+
+
+
+
 
 def main():
-    pass
+    sol = Solution()
+    # nums1 = [1, 2]
+    # nums2 = [3, 4]
+    # nums1 = [1, 3]
+    # nums2 = [2]
+    # nums1 = [1, 3]
+    # nums2 = []
+    nums1 = [3]
+    nums2 = [-2,-1]
+    print(sol.findMedianSortedArrays(nums1, nums2))
 
 
 if __name__ == "__main__":

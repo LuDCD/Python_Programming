7cae6fac31243f30df006647e4ca01cc4f621d7c
zhouheng2018@163.com <zhouheng2018@163.com>
2018-11-14 19:27:49 +0800

    123

---
+++ PracticePY/财务自由.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+财务自由
+
+每年支出 / 4% 是你要拥有的可带来增值的财富总额
+
+
+@author: ZHOU Heng
+"""
+
+
+def main():
+    year = 20  # 年
+    rate = 12 / 100  # 年化收益率
+    base = 1.2  # 本金，单位：万
+
+    money = 0
+    for i in range(year):
+        money = (base + money) * (1 + rate)
+
+    print(
+        "每年投入 {} W元，年化收益 {:2.0f}%，坚持 {} 年后，总资产 {:2.3f} 万元。".format(
+            base, rate * 100, year, money
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()

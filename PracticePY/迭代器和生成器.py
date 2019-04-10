3d2ea7afff6d419f37c34d41ac3a8d0658ead6a6
zhouheng2018@163.com <zhouheng2018@163.com>
2019-04-10 15:38:44 +0800

    test

--- PracticePY/迭代器和生成器.py
+++ PracticePY/迭代器和生成器.py
@@ -19,23 +19,25 @@ print(alist, type(alist))
 
 # 【例1】
 print("生成器【例1】：")
-agen = (i*2-5 for i in range(10))
+agen = (i * 2 - 5 for i in range(10))
 print(agen)
 for e in agen:
     print(e)
 
 # 【例2】
 print("生成器【例2】:")
+
+
 def fabi():
-    a, b = 0, 1 # 表示第cnt个斐波那契数列的数
+    a, b = 0, 1  # 表示第cnt个斐波那契数列的数
     while True:
         yield b
-        a, b = b, a+b
+        a, b = b, a + b
+
 
 f = fabi()
-print(f) # <generator object fabi at 0x000001C0980DF728>
+print(f)  # <generator object fabi at 0x000001C0980DF728>
 for i in f:
     print(i)
     if i > 100:
         break
-

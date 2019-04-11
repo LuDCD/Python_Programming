6f8f9a8a72868238d051340293f49de69c46a7e5
zhouheng2018@163.com <zhouheng2018@163.com>
2019-04-11 20:24:30 +0800

    happy

--- PracticePY/test123.py
+++ PracticePY/test123.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from collections import Iterable
+import urllib.request
 
-print(isinstance([], Iterable)) # True
+for i in range(1,10):
+    key=urllib.request.quote(i+"是什么")
+    url="http://www.baidu.com/s?wd="+key
+    req=urllib.request.Request(url)
+    data=urllib.request.urlopen(req).read()
+    fh=open("F:/python/代码/baidu"+str(i)+"是什么.html","wb")
+    fh.write(data)
+    fh.close()
+    print("F:/python/代码/baidu"+str(i)+"是什么.html")
 

208f11597a358e14d30696c328d95b4a916529c7
zhouheng2018@163.com <zhouheng2018@163.com>
2019-04-13 22:04:23 +0800

    123

--- PracticePY/test123.py
+++ PracticePY/test123.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import urllib.request
+import numpy as np
+import matplotlib.pyplot as plt
 
-for i in range(1,10):
-    key=urllib.request.quote(i+"是什么")
-    url="http://www.baidu.com/s?wd="+key
-    req=urllib.request.Request(url)
-    data=urllib.request.urlopen(req).read()
-    fh=open("F:/python/代码/baidu"+str(i)+"是什么.html","wb")
-    fh.write(data)
-    fh.close()
-    print("F:/python/代码/baidu"+str(i)+"是什么.html")
+data = np.loadtxt("data.txt")
+# print(len(data))
 
+plt.scatter(data[:,0],data[:,1])
+plt.show()

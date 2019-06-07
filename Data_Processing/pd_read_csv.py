6525e1cf9ea2b7376498494b107e26e81a93ea28
zhouheng2018@163.com <zhouheng2018@163.com>
2019-06-07 22:49:50 +0800

    端午

--- Data_Processing/pd_read_csv.py
+++ Data_Processing/pd_read_csv.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# import numpy as np
-# import matplotlib.pyplot as plt
 
 import pandas as pd
 
+# 目录含有中文，使用 engine='python'
 data = pd.read_csv("G:/t\# 空天杯\data\红外图像数据及标签\data1.txt", sep="\t", engine='python')
 # print(data)
 print(data[1,:])
